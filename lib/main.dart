880b5fc18abe3648beb591b6f6c7a8755a911598
Bryan Oltman <bryan@shorebird.dev>
2024-02-27 17:49:54 -0500

    reduce

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:palette_generator/palette_generator.dart';
-import 'dart:ui' as ui;
-import 'package:image/image.dart' as img;
 
 void main() {
   runApp(const MyApp());
@@ -38,65 +33,35 @@ class _SBExampleState extends State<SBExample> {
   final src =
       'https://www.hunterlab.com/media/images/tomoko-uji-qzoSJlPxS9k-unsplash-mi.2e16d0ba.fill-692x346.jpg';
 
-  getInternalColor() async {
-    final pg = await PaletteGenerator.fromImageProvider(
-      NetworkImage(src),
-    );
+  _getInternalColor() async {
+    print('getting network image');
+    final networkImage = NetworkImage(src);
+    print('got network image $networkImage');
+
+    print('getting palette');
+    // This line seems to be the culprit. It takes ~4s to run on a shorebird
+    // release build.
+    // 2024-02-27 17:48:41.427233-0500 Runner[582:22693] flutter: getting palette
+    // 2024-02-27 17:48:41.488404-0500 Runner[582:22690] [updater::updater] Update thread finished with status: No update
+    // 2024-02-27 17:48:45.131444-0500 Runner[582:22693] flutter: got palette
+    final pg = await PaletteGenerator.fromImageProvider(networkImage);
+    print('got palette');
+
+    print('getting dominant color');
     final domColor = pg.dominantColor?.color;
-    Color ic = domColor ?? Colors.white;
-    finalColor = ic;
+    print('got dominant color');
+
+    finalColor = domColor ?? Colors.white;
+
     setState(() {});
   }
 
   Size finalSize = const Size(0, 0);
   Size croppedSize = const Size(0, 0);
-  img.Image? croppedIMG;
-
-  getImageSize(Image image) async {
-    Completer<ui.Image> completer = Completer<ui.Image>();
-    image.image
-        .resolve(const ImageConfiguration())
-        .addListener(ImageStreamListener((ImageInfo image, bool _) {
-      completer.complete(image.image);
-    }));
-    ui.Image info = await completer.future;
-    int width = info.width;
-    int height = info.height;
-    finalSize = Size(width.toDouble(), height.toDouble());
-    setState(() {});
-  }
-
-  cropImage(Image image) async {
-    Uint8List bytes = (await NetworkAssetBundle(Uri.parse(src)).load(src))
-        .buffer
-        .asUint8List();
-    img.Image? originalImage = img.decodeImage(bytes);
-    if (originalImage == null) return;
-    img.Image croppedImage = img.copyCrop(
-      originalImage,
-      x: -90,
-      y: -200,
-      width: (finalSize.width * 0.5).toInt(),
-      height: (finalSize.height * 0.9).toInt(),
-    );
-    img.Image resizedImage = img.copyResize(
-      croppedImage,
-      width: 720,
-      height: 480,
-      maintainAspect: true,
-    );
-
-    croppedSize =
-        Size(resizedImage.width.toDouble(), resizedImage.height.toDouble());
-    croppedIMG = resizedImage;
-
-    setState(() {});
-  }
 
   @override
   void initState() {
-    getInternalColor();
-    getImageSize(Image.network(src)).then((_) => cropImage(Image.network(src)));
+    _getInternalColor();
     super.initState();
   }
 
@@ -147,21 +112,6 @@ class _SBExampleState extends State<SBExample> {
             const Text("Image Size (default)"),
             Text(
               "(${finalSize.width} x ${finalSize.height})px",
-              style: const TextStyle(fontSize: 30),
-            ),
-            const SizedBox(height: 50),
-            if (croppedIMG != null) ...[
-              SizedBox(
-                width: 300,
-                child: Image.memory(
-                  Uint8List.fromList(img.encodeJpg(croppedIMG!)),
-                ),
-              ),
-              const SizedBox(height: 5),
-            ],
-            const Text("Image Size (cropped)"),
-            Text(
-              "(${croppedSize.width} x ${croppedSize.height})px",
               style: const TextStyle(fontSize: 30),
             ),
             const SizedBox(height: 300),
