25838ccc32e9c2a8fa25fad620c9e51d57970569
Bryan Oltman <bryan@shorebird.dev>
2024-02-27 19:39:13 -0500

    more slowdown loggingg

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:palette_generator/palette_generator.dart';
+import 'package:sbslowtest/palette_generator.dart';
 
 void main() {
   runApp(const MyApp());
